9e23842e1871e9fc1e588dbee98eb6fe31f280da
soyoungJin44 <wlsthdud0412@naver.com>
2024-11-05 13:07:20 +0900

    11

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/main/main';
+import Test from './pages/admin/test';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 
           
           <Route path='/' element={<Main/>} />
+          <Route path='/Test' element={<Test/>} />
 
 
         </Routes>
