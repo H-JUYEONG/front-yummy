3cfcfc332fb780e6d9fb5bda10f9bcfcd5623246
aripos1 <ihsjjang@gmail.com>
2024-11-07 20:30:30 +0900

    123

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/main/main';
 import VenderProductList from './pages/vender/VenderProductList';
-import VenderProduct from './pages/vender/VenderProduct';
+
 import ProductDescriptionEditor from './pages/vender/ProductDescriptionEditor';
 import VenderDashboard from './pages/vender/venderDashboard';
 import VenderOption from './pages/vender/VenderOption';
 import VenderProductRegistrationForm from './pages/vender/VenderProductRegistrationForm';
 import VenderStatistics from './pages/vender/VenderStatistics';
-import ProductDescriptionEditor from './pages/vender/ProductDescriptionEditor';
+
 import UserCakeDetail from './pages/main/UserCakeDetail';
 import AdminTest from './pages/admin/AdminTest';
 
@@ -22,24 +22,24 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Main/>} />
-          <Route path='/vender/dashboard' element={<VenderDashboard/>} />
-          <Route path='/vender/productlist' element={<VenderProductList/>} />
-          <Route path='/vender/option' element={<VenderOption/>} />
-          <Route path='/vender/registrationform' element={<VenderProductRegistrationForm/>} />
-          <Route path='/ProductDescriptionEditor' element={<ProductDescriptionEditor/>} />
+          <Route path='/' element={<Main />} />
+          <Route path='/vender/dashboard' element={<VenderDashboard />} />
+          <Route path='/vender/productlist' element={<VenderProductList />} />
+          <Route path='/vender/option' element={<VenderOption />} />
+          <Route path='/vender/registrationform' element={<VenderProductRegistrationForm />} />
+          <Route path='/ProductDescriptionEditor' element={<ProductDescriptionEditor />} />
 
 
-          <Route path='/login' element={<UserLoginForm/>} />
-          <Route path='/user/signup' element={<UserSignUpForm/>} />
-          <Route path='/user/signup/succ' element={<UserSignUpSuccess/>} />
-          <Route path='/vender/signup/succ' element={<VenderSignUpSuccess/>} />
+          <Route path='/login' element={<UserLoginForm />} />
+          <Route path='/user/signup' element={<UserSignUpForm />} />
+          <Route path='/user/signup/succ' element={<UserSignUpSuccess />} />
+          <Route path='/vender/signup/succ' element={<VenderSignUpSuccess />} />
 
 
-          <Route path='/main/cakedetail' element={<UserCakeDetail/>}/>
-          <Route path='/admintest' element={<AdminTest/>} />
-          <Route path='/vender/statistics' element={<VenderStatistics/>} />
-          <Route path='/user/login' element={<UserLoginForm/>} />
+          <Route path='/main/cakedetail' element={<UserCakeDetail />} />
+          <Route path='/admintest' element={<AdminTest />} />
+          <Route path='/vender/statistics' element={<VenderStatistics />} />
+          <Route path='/user/login' element={<UserLoginForm />} />
 
         </Routes>
       </BrowserRouter>
