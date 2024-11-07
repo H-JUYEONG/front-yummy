3079fb54b1619a9ad9299474f52d81a37b8ca921
전현오 <139675173+JeffJPortfolio@users.noreply.github.com>
2024-11-07 16:43:41 +0900

    display rough

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/main/main';
-import VenderDashboard from './pages/vender/VenderDashboard';
 import VenderProductList from './pages/vender/VenderProductList';
 import VenderProduct from './pages/vender/VenderProduct';
 import ProductDescriptionEditor from './pages/vender/ProductDescriptionEditor';
@@ -16,7 +15,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Main/>} />
-          <Route path='/vender/dashboard' element={<VenderDashboard/>} />
           <Route path='/vender/productlist' element={<VenderProductList/>} />
           <Route path='/vender/product' element={<VenderProduct/>} />
           <Route path='/ProductDescriptionEditor' element={<ProductDescriptionEditor/>} />
