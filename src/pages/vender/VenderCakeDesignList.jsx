cf39d4ac1aeb12d6c2d6584fe15b38ab06447f1a
aripos1 <ihsjjang@gmail.com>
2024-11-08 16:41:50 +0900

    123

--- src/pages/vender/VenderCakeDesignList.jsx
+++ src/pages/vender/VenderCakeDesignList.jsx
@@ -36,17 +36,16 @@ const VenderCakeDesignList = () => {
                 <header className="cake-design-list-header">
                   <h2 className="cake-design-list-title">도안 리스트</h2>
 
-
-                {/* 검색과 버튼 그룹을 한 줄로 배치 */}
-                <div className="cake-design-header-right">
-                  <div className="cake-design-search">
-                    <FaSearch className="cake-design-search-icon" />
-                    <input
-                      type="text"
-                      placeholder="검색어를 입력하세요"
-                      className="cake-design-search-input"
-                    />
-                  </div>
+                  {/* 검색과 버튼 그룹을 한 줄로 배치 */}
+                  <div className="cake-design-header-right">
+                    <div className="cake-design-search">
+                      <FaSearch className="cake-design-search-icon" />
+                      <input
+                        type="text"
+                        placeholder="검색어를 입력하세요"
+                        className="cake-design-search-input"
+                      />
+                    </div>
 
                     <div className="cake-design-button-group">
                       <button
@@ -71,44 +70,45 @@ const VenderCakeDesignList = () => {
                   </div>
                 </header>
 
+                {/* 이미지 리스트 반복구간 */}
                 <div className="cake-design-list-imgs">
-                  <img src="/images/2호_일반케이크.jpg" alt="회사 로고" />
-                  <img src="/images/2호_일반케이크.jpg" alt="회사 로고" />
-                  <img src="/images/2호_일반케이크.jpg" alt="회사 로고" />
-                  <img src="/images/2호_일반케이크.jpg" alt="회사 로고" />
-                  <img src="/images/2호_일반케이크.jpg" alt="회사 로고" />
+                  {/* <img src={`${process.env.REACT_APP_API_URL}/upload/${product.imageSavedName}`} alt="회사 로고" /> */}
+                  <img
+                    src="/images/2호_일반케이크.jpg"
+                    alt="회사 로고"
+                    onClick={handleImageClick} // 이미지 클릭 시 상세 페이지로 이동
+                  />
+                  <img
+                    src="/images/2호_일반케이크.jpg"
+                    alt="회사 로고"
+                    onClick={handleImageClick}
+                  />
+                  <img
+                    src="/images/2호_일반케이크.jpg"
+                    alt="회사 로고"
+                    onClick={handleImageClick}
+                  />
+                  <img
+                    src="/images/2호_일반케이크.jpg"
+                    alt="회사 로고"
+                    onClick={handleImageClick}
+                  />
+                  <img
+                    src="/images/2호_일반케이크.jpg"
+                    alt="회사 로고"
+                    onClick={handleImageClick}
+                  />
                 </div>
 
-
-              {/* 이미지 리스트 반복구간 */}
-              <div className="cake-design-list-imgs">
-                {/* <img src={`${process.env.REACT_APP_API_URL}/upload/${product.imageSavedName}`} alt="회사 로고" /> */}
-                <img
-                  src="/images/2호_일반케이크.jpg"
-                  alt="회사 로고"
-                  onClick={handleImageClick} // 이미지 클릭 시 상세 페이지로 이동
-                />
-                <img
-                  src="/images/2호_일반케이크.jpg"
-                  alt="회사 로고"
-                  onClick={handleImageClick}
-                />
-                <img
-                  src="/images/2호_일반케이크.jpg"
-                  alt="회사 로고"
-                  onClick={handleImageClick}
-                />
-                <img
-                  src="/images/2호_일반케이크.jpg"
-                  alt="회사 로고"
-                  onClick={handleImageClick}
-                />
-                <img
-                  src="/images/2호_일반케이크.jpg"
-                  alt="회사 로고"
-                  onClick={handleImageClick}
-                />
-              </div>
+                {/* 페이지 번호 및 네비게이션 */}
+                <div className="cake-design-list-pages">
+                  <button
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    disabled={currentPage === 1}
+                    className="page-icon"
+                  >
+                    <FaAngleLeft />
+                  </button>
 
                   {[...Array(totalPages)].map((_, index) => (
                     <button
