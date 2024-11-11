52d6daf947b73ad7f3dc1ed15facea593dbc83c3
황덕룡 <hwangsee12@naver.com>
2024-11-11 13:09:43 +0900

    1111111111

--- src/pages/user/include/Header.jsx
+++ src/pages/user/include/Header.jsx
@@ -1,54 +1,99 @@
-
 import React, { useState } from 'react';
-
 import { Link } from "react-router-dom";
-
 import '../../../assets/css/user/userheaderstyle.css';
 
 const Header = () => {
-  // 실제 구현시에는 전역 상태 관리(Redux, Context API 등)나 
-  // 세션/로컬 스토리지를 통해 로그인 상태를 관리하는 것이 좋습니다
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true); // 로그인 상태를 true로 변경
+  const [userRole] = useState('user'); // 'user', 'vender', 'admin' 중 하나로 설정
 
   const handleLogout = () => {
     setIsLoggedIn(false);
     // 로그아웃 처리 로직 추가
   };
 
+  // 역할에 따른 네비게이션 메뉴 렌더링
+  const renderNavMenu = () => {
+    switch(userRole) {
+      case 'vender': //업체
+        return (
+          <ul>
+            <li><Link to="/audition">케이크 오디션</Link></li>
+            <li><Link to="/user/cakeDesign/board">도안게시판</Link></li>
+            <li><Link to="/board">토론게시판</Link></li>
+          </ul>
+        );
+      case 'admin': //관리자
+        return (
+          <ul>
+            <li><Link to="/audition">케이크 오디션</Link></li>
+            <li><Link to="/user/cakeDesign/board">도안게시판</Link></li>
+            <li><Link to="/board">토론게시판</Link></li>
+          </ul>
+        );
+      default: // 일반 사용자
+        return (
+          <ul>
+            <li><Link to="/audition">케이크 오디션</Link></li>
+            <li><Link to="/user/cakeDesign/board">도안게시판</Link></li>
+            <li><Link to="/board">토론게시판</Link></li>
+          </ul>
+        );
+    }
+  };
+
+  // 역할에 따른 액션 버튼 렌더링
+  const renderUserActions = () => {
+    if (!isLoggedIn) {
+      return (
+        <>
+          <Link to="/user/login" className="header-link">로그인</Link>
+          <Link to="/user/signup/type" className="header-link">회원가입</Link>
+        </>
+      );
+    }
+
+    switch(userRole) {
+      case 'vender':
+        return (
+          <>
+            <Link to="/vender/" className="header-link">내 업체 페이지</Link>
+            <Link to="#" onClick={handleLogout} className="header-link">로그아웃</Link>
+          </>
+        );
+      case 'admin':
+        return (
+          <>
+            <Link to="/admin/" className="header-link">관리페이지</Link>
+            <Link to="#" onClick={handleLogout} className="header-link">로그아웃</Link>
+          </>
+        );
+      default:
+        return (
+          <>
+            <Link to='/user/mypage/order' className="header-link">마이페이지</Link>
+            <Link to="#" onClick={handleLogout} className="header-link">로그아웃</Link>
+          </>
+        );
+    }
+  };
+
   return (
     <div className="header-container">
       {/* Logo */}
       <div className="logo">
-      <Link to="/user/">
-        <h1>YUMMY</h1>
-      </Link>
+        <Link to={`/user/userMainForm`}>
+          <h1>YUMMY</h1>
+        </Link>
       </div>
 
-      {/* Navigationd */}
+      {/* 메뉴는 통일해놨는데 나중에 바꾸시려면 바꾸세요 */}
       <nav className="nav-menu">
-        <ul>
-          <li><Link to="/audition">케이크 오디션</Link></li>
-          <li><Link to="/user/cakeDesign/board">도안게시판</Link></li>
-          <li><Link to="/board">토론게시판</Link></li>
-        </ul>
+        {renderNavMenu()}
       </nav>
 
-      {/* User Actions */}
+      {/* 누르면 상태 바뀌어요 */}
       <div className="user-actions">
-
-        {isLoggedIn ? (
-          <>
-            <Link to="/mypage/order" className="header-link">마이페이지</Link>
-            <Link to="#" onClick={handleLogout} className="header-link">로그아웃</Link>
-          </>
-        ) : (
-          <>
-            <Link to="/user/login" className="header-link">로그인</Link>
-            <Link to="/user/signup/type" className="header-link">회원가입</Link>
-
-          </>
-        )}
-
+        {renderUserActions()}
       </div>
     </div>
   );
