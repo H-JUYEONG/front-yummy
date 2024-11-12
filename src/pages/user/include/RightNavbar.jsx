5670ccace28be76f6fabd91e93dae89d89078e7b
aripos1 <ihsjjang@gmail.com>
2024-11-13 00:06:26 +0900

    귀여움에 다 쓰러져라~~

--- src/pages/user/include/RightNavbar.jsx
+++ src/pages/user/include/RightNavbar.jsx
@@ -5,7 +5,7 @@ import '../../../assets/css/user/rightNavbar.css';
 const RightNavbar = () => {
     return (
         <div className="cat-container">
-        <img src="/images/happy-birthday-birthday-cake.gif" alt="Moving cat" className="moving-cat" /> {/* 고양이 GIF */}
+        <img src="/images/pusheen-cat.gif" alt="Moving cat" className="moving-cat" /> {/* 고양이 GIF */}
     </div>
     );
 };
