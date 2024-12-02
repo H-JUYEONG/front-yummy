c61eae4fa6bc68d8455d01d8d63b29043016cf54
황주영 <dud9902@gmail.com>
2024-12-02 11:26:42 +0900

    마이페이지 css수정

--- src/pages/user/include/UserSidebar.jsx
+++ src/pages/user/include/UserSidebar.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
-import { FaHeart, FaUserFriends, FaCoins, FaHome, FaUser, FaShoppingCart, } from "react-icons/fa";
-import { IoCartOutline } from "react-icons/io5";
-import { MdStorefront, MdGroup } from "react-icons/md";
-import { BiSolidStore } from "react-icons/bi";
+import "../../../assets/css/user/usermain.css";
 
 const UserSidebar = () => {
+  const location = useLocation();
   const [userNickname, setUserNickname] = useState("");
   const [authUser, setAuthUser] = useState(() => {
     const user = localStorage.getItem("authUser");
@@ -34,67 +32,94 @@ const UserSidebar = () => {
     }
   }, [userId]);
 
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
   return (
     <aside id="user-wrap-side" className="float-left">
       <div className="sidebar">
-        <h2>{userNickname}님 어서오세요!</h2>
-        {/* <ul>
-          <li>
-            <Link to="/">
-              <FaHome className="icon" /> 메인 바로가기
-            </Link>
-          </li>
-        </ul> */}
-        <h3><FaShoppingCart className="icon" />나의 쇼핑내역</h3>
-        <ul>
-          <li>
-            <Link to="/user/mypage/order">주문조회</Link>
-          </li>
-          <li>
-            <Link to="/user/mypage/audition">케이크 요청 내역</Link>
-          </li>
-        </ul>
-        <h3>
-          {" "}
-          <FaUserFriends className="icon" />내 활동
-        </h3>
-        <ul>
-          <li>
-            <Link to="/user/mypage/writinglist">내가 작성한 글</Link>
-          </li>
-          <li>
-            <Link to="/user/mypage/cakeDesign/list">내가 그린 도안</Link>
-          </li>
-        </ul>
-        <h3>
-          <FaHeart className="icon" />찜 목록
-        </h3>
-        <ul>
-          <li>
-            <Link to="/user/mypage/wishlist">찜한 상품</Link>
-          </li>
-          <li>
-            <Link to="/user/mypage/cakeDesign/like/list">찜한 도안</Link>
-          </li>
-        </ul>
-        <h3>
-          <FaCoins className="icon" />
-          포인트
-        </h3>
-        <ul>
-          <li>
-            <Link to="/user/mypage/point">포인트 내역</Link>
-          </li>
-        </ul>
-        <h3>
-          <FaUser className="icon" />
-          회원정보
-        </h3>
-        <ul>
-          <li>
-            <Link to="/user/mypage/userpersonalinfoedit">회원정보 변경</Link>
-          </li>
-        </ul>
+        <h2>마이페이지</h2>
+
+        <div className="mypage-menu-list">
+          <h3>나의 쇼핑내역</h3>
+          <ul>
+            <li
+              className={
+                isActive("/user/mypage/order") ? "mypage-menu-active" : ""
+              }
+            >
+              <Link to="/user/mypage/order">주문조회</Link>
+            </li>
+            <li
+              className={
+                isActive("/user/mypage/audition") ? "mypage-menu-active" : ""
+              }
+            >
+              <Link to="/user/mypage/audition">케이크 요청 내역</Link>
+            </li>
+          </ul>
+          <h3 className="mypage-main-title">내 활동</h3>
+          <ul>
+            <li
+              className={
+                isActive("/user/mypage/writinglist") ? "mypage-menu-active" : ""
+              }
+            >
+              <Link to="/user/mypage/writinglist">내가 작성한 글</Link>
+            </li>
+            <li
+              className={
+                isActive("/user/mypage/cakeDesign/list")
+                  ? "mypage-menu-active"
+                  : ""
+              }
+            >
+              <Link to="/user/mypage/cakeDesign/list">내가 그린 도안</Link>
+            </li>
+          </ul>
+          <h3 className="mypage-main-title">찜 목록</h3>
+          <ul>
+            <li
+              className={
+                isActive("/user/mypage/wishlist") ? "mypage-menu-active" : ""
+              }
+            >
+              <Link to="/user/mypage/wishlist">찜한 상품</Link>
+            </li>
+            <li
+              className={
+                isActive("/user/mypage/cakeDesign/like/list")
+                  ? "mypage-menu-active"
+                  : ""
+              }
+            >
+              <Link to="/user/mypage/cakeDesign/like/list">찜한 도안</Link>
+            </li>
+          </ul>
+          <h3 className="mypage-main-title">포인트</h3>
+          <ul>
+            <li
+              className={
+                isActive("/user/mypage/point") ? "mypage-menu-active" : ""
+              }
+            >
+              <Link to="/user/mypage/point">포인트 내역</Link>
+            </li>
+          </ul>
+          <h3 className="mypage-main-title">회원정보</h3>
+          <ul>
+            <li
+              className={
+                isActive("/user/mypage/userpersonalinfoedit")
+                  ? "mypage-menu-active"
+                  : ""
+              }
+            >
+              <Link to="/user/mypage/userpersonalinfoedit">회원정보 변경</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </aside>
   );
